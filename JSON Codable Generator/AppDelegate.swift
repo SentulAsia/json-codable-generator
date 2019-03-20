12724dbac933d59e7b78c54f22702714dc62ac46
Zaid M. Said <zaid.shr@gmail.com>
2019-03-20 11:19:33 +0800

    Initial Commit

---
+++ JSON Codable Generator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSON Codable Generator
+//
+//  Created by Zaid Said on 20/03/2019.
+//  Copyright © 2019 Zaid Said. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
